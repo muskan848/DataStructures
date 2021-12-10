c102aa3f48ccc73168d36ac2ea53bc10be7bcd01
muskan848 <68140965+muskan848@users.noreply.github.com>
2021-12-10 16:48:34 +0530

    Create getConnectedComponents.java

---
+++ graphs/getConnectedComponents.java
@@ -0,0 +1,68 @@
+//https://www.pepcoding.com/resources/online-java-foundation/graphs/gcc-official/ojquestion
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
+      
+      boolean visit[]=new boolean[vtces];
+       
+       for(int i=0;i<vtces;i++)
+       {
+           if(visit[i]==false)
+           {
+               ArrayList<Integer> arr=new ArrayList<>();
+               dfs(graph,i,arr,visit);
+               comps.add(arr);
+           }
+           
+       }
+
+      System.out.println(comps);
+   }
+   
+   public static void dfs(ArrayList<Edge>[] graph ,int srs, ArrayList<Integer> arr,boolean visit[] )
+   {
+       arr.add(srs);
+       visit[srs]=true;
+       for(Edge e:graph[srs])
+       {
+           if(visit[e.nbr]==false)
+           {
+               dfs(graph,e.nbr,arr,visit);
+           }
+       }
+   }  
+}
