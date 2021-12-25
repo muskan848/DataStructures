bf3c3721ae2db7d1fc5e065e5ea7e94f7bacc500
muskan848 <68140965+muskan848@users.noreply.github.com>
2021-12-25 13:17:22 +0530

    Update count no of islands.java

--- graphs/count no of islands.java
+++ graphs/count no of islands.java
@@ -2,6 +2,10 @@
 //application of get connected components
 //proactive and reactive calls
 
+//Time complexity : O(M×N) where M is the number of rows and N is the number of columns.
+
+//Space complexity : worst case O(M×N) in case that the grid map is filled with lands where DFS goes by M×N deep
+
 class Solution {
     public int numIslands(char[][] grid) 
     {
