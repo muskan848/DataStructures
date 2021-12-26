7a1604482c50ea04de875b93cd5aad5ecdf2e3b0
muskan848 <68140965+muskan848@users.noreply.github.com>
2021-12-27 01:43:07 +0530

    Update Euler path and circuit.java

--- graphs and backtracking/Euler path and circuit.java
+++ graphs and backtracking/Euler path and circuit.java
@@ -1,24 +1,26 @@
 //https://practice.geeksforgeeks.org/problems/euler-circuit-and-path/1
 
+
 class Solution
 {
-    public void shortest_distance(int[][] matrix)
+    public int isEularCircuitExist(int V, ArrayList<ArrayList<Integer>> adj)
     {
-       for(int k=0;k<matrix.length;k++)
-       {
-         for(int i=0;i<matrix.length;i++)
-         {
-             for(int j=0;j<matrix[i].length;j++)
-             {
-                 if(i==k || j==k || i==j){continue;}
-                
-                 if(matrix[i][k]==-1 || matrix[k][j]==-1){continue;}
-                 
-                 if(matrix[i][j]==-1){matrix[i][j] = matrix[i][k]+matrix[k][j];}
-               
-                 matrix[i][j]=Math.min(matrix[i][j],matrix[i][k]+matrix[k][j]);
-             }
-         }
-       }
+        int o=0;
+
+        for(int i=0;i<V;i++)
+        {
+            if(adj.get(i).size()%2 != 0)
+            {
+                o++;
+            }
+        }
+        
+        if(o==2)     //semi euler or Euler path  start!=end
+          return 1;
+        else if(o==0)   // euler or euler circuit start==end
+          return 2;
+        else
+           return 0;
+       
     }
 }
